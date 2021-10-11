771f2267278e36942cfce99692177e1a3a6eca43
Alexandre Bolzon <blzn@mail.ru>
2021-10-11 11:36:45 +0200

    Change logger var

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
-import logger
+from logger import get_logger
 
 
 if __name__ == '__main__':
 
-    log = logger.get_logger(__name__)
+    logger = get_logger(__name__)
 
-    log.info('This is an %s', 'info')
-    log.error('This is an %s', 'error')
+    logger.info('This is an %s', 'info')
+    logger.error('This is an %s', 'error')
 
-    log.debug({
+    logger.debug({
         'dict': {
             'version': '1.2.3',
             'memory': 1024
         }
     })
 
-    log.analytics({'metadata': {'msg': 'sample message'}})
-    log.analytics('foo bar')
+    logger.analytics({'metadata': {'msg': 'sample message'}})
+    logger.analytics('foo bar')
 
     try:
         a = b + c
     except Exception as ex:
-        log.error('error summing %s', 'numbers', exc_info=ex)
+        logger.error('error summing %s', 'numbers', exc_info=ex)
